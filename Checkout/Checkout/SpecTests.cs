273eb1933cb9c4f423a988e107f39a6b6f620a13
sohail87 <sohaildeen73@gmail.com>
2017-11-04 12:30:25 +0000

    New Test Failing - Scanning 1 A, Total is 50

---
+++ Checkout/Checkout/SpecTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Checkout
+{
+    [TestClass]
+    public class SpecTests
+    {
+        [TestMethod]
+        public void When_Scanning_1_A_Sku_Then_Total_Is_50()
+        {
+            Assert.AreEqual(50, Scan('A'));
+        }
+
+        private int Scan(char Sku)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
