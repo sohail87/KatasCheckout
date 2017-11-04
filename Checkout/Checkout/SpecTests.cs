ea92cc6b8d833515fdb0c9bab7816cca1d5810f9
sohail87 <sohaildeen73@gmail.com>
2017-11-04 12:34:53 +0000

    Refactor - Scan method moved to new Checkout class

--- Checkout/Checkout/SpecTests.cs
+++ Checkout/Checkout/SpecTests.cs
@@ -3,18 +3,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Checkout
 {
+    public class Checkout
+    {
+        public int Scan(char Sku)
+        {
+            return 50;
+        }
+    }
+
     [TestClass]
     public class SpecTests
     {
+        private readonly Checkout _checkout = new Checkout();
+
         [TestMethod]
         public void When_Scanning_1_A_Sku_Then_Total_Is_50()
         {
-            Assert.AreEqual(50, Scan('A'));
-        }
-
-        private int Scan(char Sku)
-        {
-            return 50;
+            Assert.AreEqual(50, _checkout.Scan('A'));
         }
     }
 }
